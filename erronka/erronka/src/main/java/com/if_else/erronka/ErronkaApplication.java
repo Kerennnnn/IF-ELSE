1fed7e5571516845e04dbff359dc15b93ff4d0e7
web25hagirre <web25haguirre@imaltuna.com>
2026-03-25 13:16:15 +0100

    Initial Commit

---
+++ erronka/erronka/src/main/java/com/if_else/erronka/ErronkaApplication.java
@@ -0,0 +1,13 @@
+package com.if_else.erronka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErronkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErronkaApplication.class, args);
+	}
+
+}
---
+++ erronka/erronka/src/test/java/com/if_else/erronka/ErronkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.if_else.erronka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErronkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
