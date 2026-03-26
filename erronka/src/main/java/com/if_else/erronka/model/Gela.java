d30bafb8cd95762a222c35ffb112aaa575ef66a4
web25hagirre <web25haguirre@imaltuna.com>
2026-03-26 11:52:32 +0100

    Gela klasea

---
+++ erronka/src/main/java/com/if_else/erronka/model/Gela.java
@@ -0,0 +1,60 @@
+package com.if_else.erronka.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Gela")
+public class Gela {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id_gela;
+
+    @ManyToOne
+    @JoinColumn(name = "id_solairua")
+    private Solairua solairua;
+
+    private int gela_zenbakia;
+
+    @OneToMany(mappedBy = "gela")
+    private List<GailuElektronikoa> gailuElektronikoak;
+
+    public Gela(int gela_zenbakia, int id_gela, Solairua solairua) {
+        this.gela_zenbakia = gela_zenbakia;
+        this.id_gela = id_gela;
+        this.solairua = solairua;
+    }
+
+    public int getId_gela() {
+        return id_gela;
+    }
+
+    public void setId_gela(int id_gela) {
+        this.id_gela = id_gela;
+    }
+
+    public Solairua getSolairua() {
+        return solairua;
+    }
+
+    public void setSolairua(Solairua solairua) {
+        this.solairua = solairua;
+    }
+
+    public int getGela_zenbakia() {
+        return gela_zenbakia;
+    }
+
+    public void setGela_zenbakia(int gela_zenbakia) {
+        this.gela_zenbakia = gela_zenbakia;
+    }
+
+}
