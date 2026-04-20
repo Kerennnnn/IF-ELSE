25ce4171cbbd5ab85682d63b69cad2522a004fae
web25hagirre <web25haguirre@imaltuna.com>
2026-04-20 12:12:46 +0200

    Rolen araberako orri nagusia + access denied portaera

--- erronka/src/main/java/com/if_else/erronka/config/SecurityConfiguration.java
+++ erronka/src/main/java/com/if_else/erronka/config/SecurityConfiguration.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 public class SecurityConfiguration {
@@ -17,18 +21,53 @@ public class SecurityConfiguration {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/home").permitAll()
-                        .requestMatchers("/admin").hasRole("ADMIN")
-                        .requestMatchers("/user").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers("/login", "/img/**", "/css/**", "/js/**").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/app/**").hasAnyRole("USER", "ADMIN")
                         .anyRequest().authenticated())
                 .formLogin(form -> form
                         .loginPage("/login")
                         .loginProcessingUrl("/login")
-                        .defaultSuccessUrl("/hello", true)
+                        .successHandler(successHandler())
                         .permitAll())
-                .logout(Customizer.withDefaults());
+
+                .logout(Customizer.withDefaults())
+                .exceptionHandling(ex -> ex
+                        .accessDeniedHandler(accessDeniedHandler()));
 
         return http.build();
+    }
+
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+        return (request, response, ex) -> {
+
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+            if (auth != null &&
+                    auth.getAuthorities().stream()
+                            .anyMatch(a -> a.getAuthority().equals("ROLE_USER"))) {
+
+                response.sendRedirect("/app/gailuak");
+            } else {
+                response.sendRedirect("/login");
+            }
+        };
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler successHandler() {
+        return (request, response, authentication) -> {
+
+            boolean isAdmin = authentication.getAuthorities().stream()
+                    .anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"));
+
+            if (isAdmin) {
+                response.sendRedirect("/admin/gailuak");
+            } else {
+                response.sendRedirect("/app/gailuak");
+            }
+        };
     }
 
     @Bean
