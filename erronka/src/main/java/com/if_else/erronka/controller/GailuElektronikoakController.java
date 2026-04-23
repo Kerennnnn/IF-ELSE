e87582cef11ce325a58b461320d859ffa23b8780
Kerennnnn <web25kmateos@imaltuna.com>
2026-04-23 10:35:51 +0200

    itxura orokorra

---
+++ erronka/src/main/java/com/if_else/erronka/controller/GailuElektronikoakController.java
@@ -0,0 +1,16 @@
+package com.if_else.erronka.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class GailuElektronikoakController {
+
+    @GetMapping("/gailuak/gehitu")
+    public String gailuakGehitu() {
+        return "gailuakGehitu";
+    }
+    
+
+}
